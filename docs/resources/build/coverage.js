9c1dccb532558c745b75fd3237cee202bf7bd413
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-01-29 22:22:07 +0900

    CodeCoverage HTML出力改善

--- docs/resources/build/coverage.js
+++ docs/resources/build/coverage.js
@@ -11,6 +11,7 @@ log = log.split(LF);
 
 var start = false;
 var lines = [];
+var toal = {};
 log.forEach(function(line) {
 
     if(start && line.substr(0,'   | '.length) === '   | ') {
@@ -45,7 +46,30 @@ log.forEach(function(line) {
 
     } else if(start && line.substr(0, '                                              |'.length) === '                                              |') {
 
-//        console.log(line);
+        // ライン解析
+        line = line.split('|');
+
+        var o = {};
+
+        line.forEach(function(part, i) {
+
+            part = NX.trim(part);
+
+            if(i == 0) {
+            } else if(i == 1) {
+                o.coverage = part;
+            } else if(i == 2) {
+                o.loc = part;
+            } else if(i == 3) {
+                o.sloc = part;
+            } else if(i == 4) {
+                o.missed = part;
+            }
+
+        });
+
+        total = o;
+
     }
 
 
@@ -61,8 +85,8 @@ var output = [
     '<table border="1" width="100%" cellpadding="5" cellspacing="0" style="border:solid 1px #999999;">',
     '<tr>',
     '     <th>filename</th>',
+    '     <th>graph</th>',
     '     <th>coverage</th>',
-    '     <th width="530">graph</th>',
     '     <th>loc</th>',
     '     <th>sloc</th>',
     '     <th>missed</th>',
@@ -73,12 +97,12 @@ lines.forEach(function(o) {
 
      var line = [
         '<tr>',
-        '     <td>%s</td>',
-        '     <td>%s</td>',
-        '     <td width="530"><img width="%s%" height="16" alt="graph" src="http://art24.photozou.jp/pub/253/198253/photo/64702984.gif"></td>',
-        '     <td>%s</td>',
-        '     <td>%s</td>',
-        '     <td>%s</td>',
+        '     <td width="312">%s</td>',
+        '     <td width="550"><img width="%s%" height="16" alt="graph" src="http://art24.photozou.jp/pub/253/198253/photo/64702984.gif" /></td>',
+        '     <td width="36" align="right">%s</td>',
+        '     <td width="36" align="right">%s</td>',
+        '     <td width="36" align="right">%s</td>',
+        '     <td width="36" align="right">%s</td>',
         '</tr>'
     ].join(LF);
 
@@ -87,6 +111,20 @@ lines.forEach(function(o) {
     output += line;
 
 });
+
+var line = [
+    '<tr>',
+    '     <td width="862" colspan="2"> </td>',
+    '     <td width="36" align="right"><strong>%s</strong></td>',
+    '     <td width="36" align="right"><strong>%s</strong></td>',
+    '     <td width="36" align="right"><strong>%s</strong></td>',
+    '     <td width="36" align="right"><strong>%s</strong></td>',
+    '</tr>'
+].join(LF);
+
+line = NX.sprintf(line, total.coverage, total.loc, total.sloc, total.missed);
+
+output += line;
 
 output += '</table>';
 
