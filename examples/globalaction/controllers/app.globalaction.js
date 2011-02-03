6a22cf18666669de9be2b7e393268ddb4789607a
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-02-03 14:20:40 +0900

    グローバルアクション実装
    サイト全体にアクセスしたときに、必ず実行されるアクションで、アクションチェーンとして利用することで、複数のアクションを事前に実行することが可能です。
    HTTPS判定や、初期化処理に利用します。

---
+++ examples/globalaction/controllers/app.globalaction.js
@@ -0,0 +1,35 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * Next JS
+ * Copyright(c) 2006-2011 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
+
+// {{{ globalaction
+
+module.exports = NX.extend(NX.Action, {
+
+    // {{{ execute
+
+    execute : function() {
+
+        var me = this;
+
+        me.set('globalaction', 'app.global.action executed');
+        me.end();
+    }
+
+    // }}}
+
+});
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
