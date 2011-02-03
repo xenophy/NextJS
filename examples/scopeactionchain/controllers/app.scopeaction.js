22e02fcdd333fc0960104cbd66dbc53c86b58e45
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-02-03 15:19:22 +0900

    スコープアクション実装
    xFramework PX(以下PX)のアクションスタックは、このグローバルアクションにアクションチェーンを設定することで、実現する。
    スコープアクションは、PX同様配置されたディレクトリ以下に適用され、子階層で指定されて場合、親のスコープアクションを上書きします。
    ディレクトリ以下に、同一のアクションを実行したい場合や、ログイン処理に利用します。

---
+++ examples/scopeactionchain/controllers/app.scopeaction.js
@@ -0,0 +1,27 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * Next JS
+ * Copyright(c) 2006-2011 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
+
+// {{{ scopeaction
+
+module.exports = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five'
+];
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
---
+++ examples/scopeactionchain/controllers/subdir1/override/app.scopeaction.js
@@ -0,0 +1,35 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * Next JS
+ * Copyright(c) 2006-2011 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
+
+// {{{ scopeaction
+
+module.exports = NX.extend(NX.Action, {
+
+    // {{{ execute
+
+    execute : function() {
+
+        var me = this;
+
+        me.set('scopeaction', 'subdir1/override/app.scopeaction executed');
+        me.end();
+    }
+
+    // }}}
+
+});
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
