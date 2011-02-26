b32d3250ad4c6e89bf5cb3d13e218ba2922d5b73
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-02-26 19:05:19 +0900

    NX.Array.clean ユニットテスト追加

---
+++ test/Array/clean.test.js
@@ -0,0 +1,79 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * Next JS
+ * Copyright (c)2011 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
+
+// {{{ requires
+
+require('should');
+var assert = require('assert');
+
+var T_Array = require('NX/Array');
+
+// }}}
+// {{{ clean
+
+module.exports = {
+
+    // {{{ test clean#pattern1
+
+    'test clean#pattern1': function() {
+
+        var ret = T_Array.clean([0, 1, 2, 3, 4]);
+
+        ret[0].should.equal(0);
+        ret[1].should.equal(1);
+        ret[2].should.equal(2);
+        ret[3].should.equal(3);
+        ret[4].should.equal(4);
+        ret.length.should.equal(5);
+    },
+
+    // }}}
+    // {{{ test toArray#pattern2
+
+    'test toArray#pattern2': function() {
+
+        var ret = T_Array.clean([0, "", 2, "", 4]);
+
+        ret[0].should.equal(0);
+        ret[1].should.equal(2);
+        ret[2].should.equal(4);
+        ret.length.should.equal(3);
+
+    },
+
+    // }}}
+    // {{{ test toArray#pattern3
+
+    'test toArray#pattern3': function() {
+
+        var ret = [0, 1, 2, 3, 4];
+        ret[0] = '';
+        ret[4] = '';
+
+        ret = T_Array.clean(ret);
+
+        ret[0].should.equal(1);
+        ret[1].should.equal(2);
+        ret[2].should.equal(3);
+        ret.length.should.equal(3);
+
+    }
+
+    // }}}
+
+};
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
