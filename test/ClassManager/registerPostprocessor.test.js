3bf796fb81f3c61b6fc9e972ee759be8ab958b2b
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-02-28 14:16:15 +0900

    postprocessor/defaultPostprocessorを完全にプライベート化

---
+++ test/ClassManager/registerPostprocessor.test.js
@@ -0,0 +1,57 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * Next JS
+ * Copyright (c)2011 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
+
+// {{{ requires
+
+require('should');
+var assert = require('assert');
+
+var T_NX = require('NX/core');
+var T_ClassManager = require('NX/ClassManager');
+
+// }}}
+// {{{ registerPostprocessor
+
+module.exports = {
+
+    // {{{ test registerPostprocessor#pattern1
+
+    'test registerPostprocessor#pattern1': function() {
+
+        var foo = function() {
+            return 'foo';
+        };
+
+        var bar = function() {
+            return 'bar';
+        };
+
+        T_ClassManager.registerPostprocessor({
+            foo: foo,
+            bar: bar
+        });
+
+        assert.equal(T_ClassManager.getPostprocessor('foo'), foo);
+        assert.equal(T_ClassManager.getPostprocessor('bar'), bar);
+        assert.equal(T_ClassManager.getPostprocessor('foo')(), foo());
+        assert.equal(T_ClassManager.getPostprocessor('bar')(), bar());
+    },
+
+    // }}}
+
+};
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
