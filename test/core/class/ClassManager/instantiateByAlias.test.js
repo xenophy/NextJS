516e9ebc95437956162d4d9688db2ebd6426cb85
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-03-01 22:56:53 +0900

    instantiateByAlias ユニットテスト追加

---
+++ test/core/class/ClassManager/instantiateByAlias.test.js
@@ -0,0 +1,78 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * Next JS
+ * Copyright (c)2011 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
+
+// {{{ requires
+
+require('should');
+var assert = require('assert');
+
+var T_NX = require('NX/core');
+var T_Array = require('NX/core/lang/Array');
+var T_ClassManager = require('NX/core/class/ClassManager');
+
+// }}}
+// {{{ instantiateByAlias
+
+module.exports = {
+
+    // {{{ test instantiateByAlias#pattern1
+
+    'test instantiateByAlias#pattern1': function() {
+
+        T_ClassManager.setDefaultPostprocessors([]);
+
+        T_ClassManager.create('ClassManagerTest.instantinateByAlias.cls1', {
+            test: function() {
+                return 'test';
+            }
+        });
+
+        T_ClassManager.setAlias('ClassManagerTest.instantinateByAlias.cls1', 'ClassManagerTest.instantinateByAlias.cls2');
+
+        var cls2 = T_ClassManager.instantiateByAlias('ClassManagerTest.instantinateByAlias.cls2');
+        cls2.test().should.equal('test');
+
+    },
+
+    // }}}
+    // {{{ test instantiateByAlias#pattern2
+
+    'test instantiateByAlias#pattern2': function() {
+
+        T_ClassManager.setDefaultPostprocessors([]);
+
+        T_ClassManager.create('ClassManagerTest.instantinateByAlias.cls1', {
+            constructor: function(config) {
+                T_NX.apply(this, config);
+                return this;
+            },
+            test: function() {
+                return this.foo;
+            }
+        });
+
+        T_ClassManager.setAlias('ClassManagerTest.instantinateByAlias.cls1', 'ClassManagerTest.instantinateByAlias.cls2');
+
+        var cls2 = T_ClassManager.instantiateByAlias('ClassManagerTest.instantinateByAlias.cls2', {foo: 'bar'});
+        cls2.test().should.equal('bar');
+
+    },
+
+    // }}}
+
+};
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
