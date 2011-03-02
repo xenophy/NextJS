753c8726e60d40c7aed09fbd8cb995ec49cd755c
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-03-03 07:53:03 +0900

    NX.util.Event テスト追加

--- test/core/util/Event/addListener.test.js
+++ test/core/util/Event/addListener.test.js
@@ -32,6 +32,143 @@ module.exports = {
 
         ev.listeners.length.should.equal(1);
 
+    },
+
+    // }}}
+    // {{{ test addListener#pattern2
+
+    'test addListener#pattern2': function(beforeExit) {
+
+        var startTime = new Date();
+
+        var fc = 0;
+        var fc2 = 0;
+
+        var f = function() {
+            var endTime = new Date();
+            var msec = endTime - startTime;
+
+            msec.should.be.within(10,100);
+            fc++;
+        };
+
+        var f2 = function() {
+            var endTime = new Date();
+            var msec = endTime - startTime;
+
+            msec.should.be.within(10,100);
+            fc2++;
+        };
+
+        var ev = new T_UTIL_Event();
+
+        ev.addListener(f, f, {
+            delay: 10
+        });
+
+        ev.addListener(f2, f2, {
+            delay: 10
+        });
+
+        ev.fire();
+        ev.fire();
+
+        beforeExit(function() {
+            fc.should.equal(2);
+            fc2.should.equal(2);
+        });
+    },
+
+    // }}}
+    // {{{ test addListener#pattern3
+
+    'test addListener#pattern3': function(beforeExit) {
+
+        var startTime = new Date();
+
+        var fc = 0;
+        var fc2 = 0;
+
+        var f = function() {
+            var endTime = new Date();
+            var msec = endTime - startTime;
+
+            msec.should.be.within(10,100);
+            fc++;
+        };
+
+        var f2 = function() {
+            var endTime = new Date();
+            var msec = endTime - startTime;
+
+            msec.should.be.within(10,100);
+            fc2++;
+        };
+
+        var ev = new T_UTIL_Event();
+
+        ev.addListener(f, f, {
+            buffer: 10
+        });
+
+        ev.addListener(f2, f2, {
+            buffer: 10
+        });
+
+        ev.fire();
+        ev.listeners.length.should.equal(2);
+        ev.fire();
+
+        beforeExit(function() {
+            fc.should.equal(1);
+            fc2.should.equal(1);
+        });
+    },
+
+    // }}}
+    // {{{ test addListener#pattern4
+
+    'test addListener#pattern4': function(beforeExit) {
+
+        var startTime = new Date();
+
+        var fc = 0;
+        var fc2 = 0;
+
+        var f = function() {
+            var endTime = new Date();
+            var msec = endTime - startTime;
+
+            msec.should.be.within(0,100);
+            fc++;
+        };
+
+        var f2 = function() {
+            var endTime = new Date();
+            var msec = endTime - startTime;
+
+            msec.should.be.within(0,100);
+            fc2++;
+        };
+
+        var ev = new T_UTIL_Event();
+
+        ev.addListener(f, f, {
+            single: true
+        });
+
+        ev.addListener(f2, f2, {
+            single: true
+        });
+
+        ev.fire();
+        ev.listeners.length.should.equal(0);
+        ev.fire();
+
+        beforeExit(function() {
+            fc.should.equal(1);
+            fc2.should.equal(1);
+        });
     }
 
     // }}}
