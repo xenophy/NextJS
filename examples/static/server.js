537e2fe419dfd4f6eb7835272e7ad777349049b4
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-03-10 19:35:30 +0900

    NXD(Next JS Daemon)をC++でnode.jsアドオンとして実装
    あわせて、NX.server.Daemonクラス実装

---
+++ examples/static/server.js
@@ -0,0 +1,28 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * Next JS
+ * Copyright (c)2011 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
+
+// {{{ requires
+
+require('../../lib/NX');
+
+// }}}
+// {{{ HTTP Server
+
+NX.regServer('http', {
+
+});
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
