c689295dca4221b141e2037f4ca1e5cc29291b4c
Yuki Asano <asano@xenophy.com>
2011-03-16 13:33:17 +0900

    callParentテスト追加

--- test/core/class/Base/callParent.test.js
+++ test/core/class/Base/callParent.test.js
@@ -11,6 +11,7 @@
 require('should');
 var assert = require('assert');
 
+var NX = require('NX/core');
 require('NX/core/class/ClassManager');
 var T_Class = require('NX/core/class/Class');
 
@@ -58,6 +59,33 @@ module.exports = {
         } catch(e) {
             e.message.should.equal("[NXTest.Base.callParent.p1cls#hoge] this.parent was called but there's no such method (hoge) found in the parent class (NX.Base)");
         }
+
+    },
+
+    // }}}
+    // {{{ test callParent#pattern3
+
+    'test callParent#pattern3': function() {
+
+         NX.define('NXTest.Base.callParent.p3cls1', {
+             config: {
+                 name: 'test'
+             },
+             constructor: function(config) {
+                 this.initConfig(config);
+                 return 'hoge';
+             }
+        });
+
+        NX.define('NXTest.Base.callParent.p3cls2', {
+            extend: 'NXTest.Base.callParent.p3cls1',
+            constructor : function() {
+                var test = this.callParent(arguments);
+                test.should.equal('hoge');
+            }
+        });
+
+        var cls = new NXTest.Base.callParent.p3cls2();
 
     }
 
