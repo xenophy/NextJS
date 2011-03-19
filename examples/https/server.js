896830c5f3c5055ae4573449762bc6d5ce47781d
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-03-20 04:23:37 +0900

    SSL対応
    複数サーバー指定によるNX.Cluster/NX.server.Daemon対応が
    完了していないためシングルプロセスで動作しますが、開発中のものです。

--- examples/https/server.js
+++ examples/https/server.js
@@ -1,15 +1,56 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
+/*!
+ * Next JS
+ * Copyright (c)2011 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
 
+// {{{ requires
 
-var https = require('https');
-var fs = require('fs');
+require('../../lib/NX');
 
-var options = {
-  key: fs.readFileSync('./certs/privatekey.pem'),
-  cert: fs.readFileSync('./certs/certificate.pem')
-};
+// }}}
+// {{{ HTTP Server
 
-https.createServer(options, function (req, res) {
-  res.writeHead(200);
-  res.end("hello world\n");
-}).listen(8000);
+/**
+ * WELL KNOWN PORT NUMBERS(0-1023)を指定する場合はroot権限が必要かも知れません。
+ * 次のコマンドで起動するしてください。
+ *
+ * sudo node server.js start
+ *
+ * http://localhost/
+ * https://localhost/
+ *
+ * で動作確認が行えます。
+ * また、Apache / Skypeなどとポートがかぶる場合は起動できませんのでご注意ください。
+ */
+NX.regServer([{
+
+    // ポート指定
+    port: 80
+
+},{
+
+    // SSL設定、ポート番号は設定しないと自動的に443になります。
+    ssl: {
+
+        // 秘密鍵パス設定
+        key: './certs/privatekey.pem',
+
+        // 証明書パス設定
+        cert: './certs/certificate.pem'
+
+    }
+
+}]);
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
