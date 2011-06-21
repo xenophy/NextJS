8ed0e462b7af8587244617c656a64a9c02b5d2ac
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-06-21 22:38:55 +0900

    ユニットテスト追加

--- test/Object.test.js
+++ test/Object.test.js
@@ -312,6 +312,101 @@ module.exports = {
         var ret = NX.Object.getKey(o, 5);
         assert.equal(ret, null);
 
+    },
+
+    // }}}
+    // {{{ test toQueryObjects#pattern1
+
+    'test toQueryObjects#pattern1': function() {
+
+        var ret = NX.Object.toQueryObjects('hobbies', ['reading', 'cooking', 'swimming']);
+
+        ret[0].name.should.equal('hobbies');
+        ret[0].value.should.equal('reading');
+        ret[1].name.should.equal('hobbies');
+        ret[1].value.should.equal('cooking');
+        ret[2].name.should.equal('hobbies');
+        ret[2].value.should.equal('swimming');
+
+    },
+
+    // }}}
+    // {{{ test toQueryObjects#pattern2
+
+    'test toQueryObjects#pattern2': function() {
+
+        var o = NX.Object.toQueryObjects(
+            'dateOfBirth',
+            {
+                day: 3,
+                month: 8,
+                year: 1987,
+                extra: {
+                    hour: 4,
+                    minute: 30
+                }
+            },
+            true
+        );
+
+        o[0].name.should.equal('dateOfBirth[day]');
+        o[0].value.should.equal(3);
+        o[1].name.should.equal('dateOfBirth[month]');
+        o[1].value.should.equal(8);
+        o[2].name.should.equal('dateOfBirth[year]');
+        o[2].value.should.equal(1987);
+        o[3].name.should.equal('dateOfBirth[extra][hour]');
+        o[3].value.should.equal(4);
+        o[4].name.should.equal('dateOfBirth[extra][minute]');
+        o[4].value.should.equal(30);
+
+    },
+
+    // }}}
+    // {{{ test toQueryObjects#pattern3
+
+    'test toQueryObjects#pattern3': function() {
+
+        var o = NX.Object.toQueryObjects(
+            'dateOfBirth',
+            {
+                day: 3,
+                month: 8,
+                year: 1987,
+                extra: {
+                    hour: 4,
+                    minute: 30
+                }
+            },
+            false
+        );
+
+        o[0].name.should.equal('dateOfBirth');
+        o[0].value.should.equal(3);
+        o[1].name.should.equal('dateOfBirth');
+        o[1].value.should.equal(8);
+        o[2].name.should.equal('dateOfBirth');
+        o[2].value.should.equal(1987);
+        o[3].name.should.equal('dateOfBirth');
+        o[3].value.hour.should.equal(4);
+        o[3].value.minute.should.equal(30);
+
+    },
+
+    // }}}
+    // {{{ test toQueryObjects#pattern4
+
+    'test toQueryObjects#pattern4': function() {
+
+        var ret = NX.Object.toQueryObjects('hobbies', ['reading', 'cooking', 'swimming'], true);
+
+        ret[0].name.should.equal('hobbies[0]');
+        ret[0].value.should.equal('reading');
+        ret[1].name.should.equal('hobbies[1]');
+        ret[1].value.should.equal('cooking');
+        ret[2].name.should.equal('hobbies[2]');
+        ret[2].value.should.equal('swimming');
+
     }
 
     // }}}
