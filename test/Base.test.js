bede408aaf13d59559059388950c93ed0129e8e8
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-06-22 23:32:28 +0900

    NX.Baseユニットテスト追加

--- test/Base.test.js
+++ test/Base.test.js
@@ -21,7 +21,6 @@ module.exports = {
 
     'test callOverridden#pattern1': function() {
 
-        /*
         NX.ClassManager.create('NXTest.Base.callOverridden.p1cls', {
             hoge: function() {
                 return 'org';
@@ -39,9 +38,8 @@ module.exports = {
             var cls = new NXTest.Base.callOverridden.p1cls2();
             cls.hoge();
         } catch(e) {
-            e.message.should.equal("[NXTest.Base.callOverridden.p1cls2] this.callOverridden was called in 'hoge' but this method has never been overridden");
+            e.message.should.equal('this.callOverridden was called in \'hoge\' but this method has never been overridden');
         }
-        */
 
     },
 
@@ -50,24 +48,209 @@ module.exports = {
 
     'test callOverridden#pattern2': function() {
 
-        /*
         NX.ClassManager.create('NXTest.Base.callOverridden.p2cls', {
             hoge: function() {
                 return 'org';
             }
         });
 
-        NXTest.Base.callOverridden.p2cls.override('hoge', function() {
-            this.callOverridden().should.equal('org');
+        NXTest.Base.callOverridden.p2cls.override({
+            hoge: function() {
+                this.callOverridden().should.equal('org');
+            }
         });
 
         var cls = new NXTest.Base.callOverridden.p2cls();
         cls.hoge();
-        */
 
     },
 
     // }}}
+    // {{{ test callOverridden#pattern3
+
+    'test callOverridden#pattern3': function() {
+
+        NX.ClassManager.create('NXTest.Base.callOverridden.p3cls', {
+            hoge: function() {
+                return 'org';
+            }
+        });
+
+        var me = this;
+        var cls = new NXTest.Base.callOverridden.p3cls();
+
+        NXTest.Base.callOverridden.p3cls.override({
+            hoge: function() {
+
+                NXTest.Base.callOverridden.p3cls.prototype.hoge['$owner'] = null;
+
+                try{
+                    this.callOverridden().should.equal('org');
+                } catch(e) {
+                    e.message.should.equal("Attempting to call a protected method from the public scope, which is not allowed");
+                }
+            }
+        });
+
+        cls.hoge();
+
+    },
+
+    // }}}
+    // {{{ test callParent#pattern1
+
+    'test callParent#pattern1': function() {
+
+        NX.ClassManager.create('NXTest.Base.callParent.p1cls', {
+            exntend: function() {
+            },
+            hoge: function() {
+                this.callParent();
+            }
+        });
+
+        try {
+            var cls = new NXTest.Base.callParent.p1cls();
+            cls.hoge();
+        } catch(e) {
+            e.message.should.equal('this.callParent() was called but there\'s no such method (hoge) found in the parent class (NX.Base)');
+        }
+
+    },
+
+    // }}}
+    // {{{ test callParent#pattern2
+
+    'test callParent#pattern2': function() {
+
+        NX.ClassManager.create('NXTest.Base.callParent.p2cls', {
+            exntend: false,
+            hoge: function() {
+                this.callParent();
+            }
+        });
+
+        try {
+            var cls = new NXTest.Base.callParent.p1cls();
+            cls.hoge();
+        } catch(e) {
+            e.message.should.equal('this.callParent() was called but there\'s no such method (hoge) found in the parent class (NX.Base)');
+        }
+
+    },
+
+    // }}}
+    // {{{ test callParent#pattern3
+
+    'test callParent#pattern3': function() {
+
+         NX.define('NXTest.Base.callParent.p3cls1', {
+             config: {
+                 name: 'test'
+             },
+             constructor: function(config) {
+                 this.initConfig(config);
+                 return 'hoge';
+             }
+        });
+
+        NX.define('NXTest.Base.callParent.p3cls2', {
+            extend: 'NXTest.Base.callParent.p3cls1',
+            constructor : function() {
+                var test = this.callParent(arguments);
+                test.should.equal('hoge');
+            }
+        });
+
+        var cls = new NXTest.Base.callParent.p3cls2();
+
+    },
+
+    // }}}
+    // {{{ test callParent#pattern4
+
+    'test callParent#pattern4': function() {
+
+         NX.define('NXTest.Base.callParent.p4cls1', {
+             config: {
+                 name: 'test'
+             },
+             constructor: function(config) {
+                 this.initConfig(config);
+                 return 'hoge';
+             }
+        });
+
+        NX.define('NXTest.Base.callParent.p4cls2', {
+            extend: 'NXTest.Base.callParent.p4cls1',
+            constructor : function() {
+                var test = this.callParent(arguments);
+                test.should.equal('hoge');
+            }
+        });
+
+        var cls = new NXTest.Base.callParent.p4cls2();
+
+        try{
+            cls.callParent.call({
+                callParent: {
+                    caller: {}, '$owner': null
+                }
+            });
+        } catch(e) {
+            e.message.should.equal('Attempting to call a protected method from the public scope, which is not allowed');
+        }
+
+    },
+
+    // }}}
+    // {{{ test callParent#pattern5
+
+    'test callParent#pattern5': function() {
+
+         NX.define('NXTest.Base.callParent.p5cls1', {
+             config: {
+                 name: 'test'
+             },
+             constructor: function(config) {
+                 this.initConfig(config);
+                 return 'hoge';
+             }
+        });
+
+        NX.define('NXTest.Base.callParent.p5cls2', {
+            extend: 'NXTest.Base.callParent.p5cls1',
+            constructor : function() {
+                var test = this.callParent(arguments);
+                test.should.equal('hoge');
+            }
+        });
+
+        var cls = new NXTest.Base.callParent.p5cls2();
+
+        try{
+            cls.callParent.call({
+                callParent: {
+                    caller: {
+                        caller: {
+                            '$owner' :{
+                                superclass: {}
+                            }
+                        }
+                    },
+                    '$owner': null
+                }
+            });
+        } catch(e) {
+            e.message.should.equal('this.callParent() was called but there\'s no such method (undefined) found in the parent class (Object)');
+        }
+
+
+    },
+
+    // }}}
+
+
 
 };
 
