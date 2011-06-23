cde327ca30c5d427a941091e08cca0eccab6c275
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-06-23 12:02:16 +0900

    NX.Baseユニットテスト追加（100%）

--- test/Base.test.js
+++ test/Base.test.js
@@ -249,8 +249,270 @@ module.exports = {
     },
 
     // }}}
+    // {{{ test setConfig#pattern1
 
+    'test setConfig#pattern1': function() {
 
+         NX.define('NXTest.Base.setConfig.p1cls1', {
+             config: {
+                 name: 'test'
+             },
+             constructor: function(config) {
+                 this.initConfig(config);
+                 return 'hoge';
+             }
+        });
+
+        var cls = new NXTest.Base.setConfig.p1cls1();
+
+        cls.name.should.equal('test');
+
+        cls.setConfig({
+            name: 'foo'
+        });
+
+        cls.name.should.equal('foo');
+
+    },
+
+    // }}}
+    // {{{ test statics#pattern1
+
+    'test statics#pattern1': function() {
+
+        NX.ClassManager.create('NXTest.Base.statics.p1cls', {
+            statics: {
+                fee: 'baz',
+            },
+            hoge: function() {
+                this.statics().fee.should.equal('baz');
+            }
+        });
+
+        NX.ClassManager.create('NXTest.Base.statics.p1cls2', {
+            extend: 'NXTest.Base.statics.p1cls',
+            statics: {
+                poo: 'par'
+            },
+            hoge2: function() {
+                assert.equal(this.statics().fee, undefined);
+                assert.equal(this.statics().poo, 'par');
+
+                var ret = this.statics.call({statics: {}});
+                assert.equal(ret, undefined);
+
+            }
+        });
+
+        var cls = new NXTest.Base.statics.p1cls2();
+
+        cls.hoge();
+        cls.hoge2();
+
+    },
+
+    // }}}
+    // {{{ test addStatics#pattern1
+
+    'test addStatics#pattern1': function() {
+
+        NX.ClassManager.create('NXTest.Base.addStatics.p1cls', {
+            statics: {
+                fee: 'baz',
+            },
+            hoge: function() {
+                this.statics().fee.should.equal('baz');
+                this.statics().foo().should.equal('bar');
+            }
+        });
+
+        NXTest.Base.addStatics.p1cls.addStatics({
+            foo: function() {
+                return 'bar';
+            }
+        });
+
+        var cls = new NXTest.Base.addStatics.p1cls();
+
+    },
+
+    // }}}
+    // {{{ test borrow#pattern1
+
+    'test borrow#pattern1': function() {
+
+        NX.ClassManager.create('NXTest.Base.borrow.p1cls', {
+            money: '$$$',
+            printMoney: function() {
+                return '$$$$$$$';
+            }
+        });
+
+        NX.ClassManager.create('NXTest.Base.borrow.p1cls2', {
+        });
+
+        NXTest.Base.borrow.p1cls2.borrow(NXTest.Base.borrow.p1cls, ['money', 'printMoney']);
+
+        var steve = new NXTest.Base.borrow.p1cls2();
+
+        steve.money.should.equal('$$$');
+        steve.printMoney().should.equal('$$$$$$$');
+
+    },
+
+    // }}}
+    // {{{ test create#pattern1
+
+    'test create#pattern1': function() {
+
+        NX.ClassManager.create('NXTest.Base.create.p1cls', {
+            foo: 'bar',
+            constructor: function(config) {
+                NX.apply(this, config || {});
+            }
+        });
+
+        var cls = NXTest.Base.create.p1cls.create({foo: 'piyo'});
+        cls.foo.should.equal('piyo');
+
+    },
+
+    // }}}
+    // {{{ test createAlias#pattern1
+
+    'test createAlias#pattern1': function() {
+
+        NX.define('NXTest.Base.createAlias.p1cls', {
+            method1: function() {
+                return 'method1';
+            },
+            method2: function() {
+                return 'method2';
+            }
+        });
+
+        var test = new NXTest.Base.createAlias.p1cls();
+
+        NXTest.Base.createAlias.p1cls.createAlias({
+            method3: 'method1',
+            method4: 'method2'
+        });
+
+        test.method3().should.equal('method1');
+
+        NXTest.Base.createAlias.p1cls.createAlias('method5', 'method3');
+
+        test.method5().should.equal('method1');
+
+    },
+
+    // }}}
+    // {{{ test getName#pattern1
+
+    'test getName#pattern1': function() {
+
+        NX.define('NXTest.Base.getName.p1cls', {
+        });
+
+        NXTest.Base.getName.p1cls.getName().should.equal('NXTest.Base.getName.p1cls');
+    },
+
+    // }}}
+    // {{{ test mixin#pattern1
+
+    'test mixin#pattern1': function() {
+
+        NX.define('NXTest.Base.mixin.p1mixin', {
+            foo: 'bar',
+            hoge: function() {
+                return 'piyo';
+            }
+        });
+
+        NX.define('NXTest.Base.mixin.p1cls', {
+            p1cls: function() {
+                return  'p1cls';
+            }
+        });
+
+        NX.define('NXTest.Base.mixin.p1cls2', {
+            extend: 'NXTest.Base.mixin.p1cls',
+            p1cls2: function() {
+                return  'p1cls2';
+            }
+        });
+
+        NXTest.Base.mixin.p1cls2.mixin('NXTest.Base.mixin.p1mixin', NXTest.Base.mixin.p1mixin);
+
+        cls = new NXTest.Base.mixin.p1cls2();
+        cls.p1cls().should.equal('p1cls');
+        cls.p1cls2().should.equal('p1cls2');
+        cls.foo.should.equal('bar');
+        cls.hoge().should.equal('piyo');
+
+    },
+
+    // }}}
+    // {{{ test mixin#pattern2
+
+    'test mixin#pattern2': function() {
+
+        NX.define('NXTest.Base.mixin.p2cls', {
+            p2cls: function() {
+                return  'p2cls';
+            }
+        });
+
+        NX.define('NXTest.Base.mixin.p2cls2', {
+            extend: 'NXTest.Base.mixin.p2cls',
+            p2cls2: function() {
+                return  'p2cls2';
+            }
+        });
+
+        var f = function() {};
+        f.prototype = {
+            foo: 'bar',
+            hoge: function() {
+                return 'piyo';
+            }
+        };
+
+        NXTest.Base.mixin.p2cls2.mixin('NXTest.Base.mixin.p2mixin', f);
+
+        cls = new NXTest.Base.mixin.p2cls2();
+        cls.p2cls().should.equal('p2cls');
+        cls.p2cls2().should.equal('p2cls2');
+
+        cls.foo.should.equal('bar');
+        cls.hoge().should.equal('piyo');
+    },
+
+    // }}}
+    // {{{ test override#pattern1
+
+    'test override#pattern1': function() {
+
+        NX.define('NXTest.Base.override.p1cls', {
+            hoge: function() {
+                return  'piyo';
+            },
+            foo: 123
+        });
+        NXTest.Base.override.p1cls.override({
+            hoge: function() {
+                return this.callOverridden() + '2';
+            },
+            foo: 'bar'
+        });
+
+        var cls = new NXTest.Base.override.p1cls();
+        cls.hoge().should.equal('piyo2');
+        cls.foo.should.equal('bar');
+
+    }
+
+    // }}}
 
 };
 
