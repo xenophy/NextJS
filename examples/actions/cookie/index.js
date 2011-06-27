4b3265b19dc61a775365e27141b7f2da95e765a5
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-06-28 06:25:44 +0900

    Cookieの設定、取得実装完了

---
+++ examples/actions/cookie/index.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+
+    execute : function() {
+
+        this.set('nickname', this.cookie.param1);
+
+
+        this.setCookie('param1', 'kotsutsumi', {
+            expires: '2020/05/16'
+        });
+
+        this.end();
+    }
+
+};
+
