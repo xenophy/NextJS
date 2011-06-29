bcd4f20c04ec8bda218d4036c963440a48805e27
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-06-29 17:20:00 +0900

    セッション実装中
    reqインスタンスをわざわざ毎回別にするようにしたのに、はまり中など。

--- examples/actions/global.action.js
+++ examples/actions/global.action.js
@@ -3,6 +3,11 @@ module.exports = {
 
     execute : function() {
 
+        /*
+        this.req.session.aaa = 'teston';
+        console.log(this.req.session.aaa);
+        */
+
         console.log("globalaction");
 
         this.end();
