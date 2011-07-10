289ca08ee82e14350f0a0ae173a18d2e14589c7e
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-07-11 05:59:14 +0900

    ユニットテスト追加

--- test/NX.test.js
+++ test/NX.test.js
@@ -1467,6 +1467,25 @@ module.exports = {
             }
         });
 
+    },
+
+    // }}}
+    // {{{ test getRandomInt#pattern1
+
+    'test getRandomInt#pattern1': function() {
+
+        var n = NX.getRandomInt(0, 1024);
+        if(n > 1024) { assert.ok(false); }
+
+        var n = NX.getRandomInt(0, 0);
+        if(n > 0) { assert.ok(false); }
+        n.should.equal(0);
+
+        var n = NX.getRandomInt(0, 1);
+        if(n > 1) { assert.ok(false); }
+
+        var n = NX.getRandomInt(1, 0);
+        n.should.equal(1);
     }
 
     // }}}
