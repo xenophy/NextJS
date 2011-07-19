3101c322305890dc8e34cb26c049aaf5282002c5
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-07-20 06:50:03 +0900

    NX.Dateユニットテスト追加

--- test/Date.test.js
+++ test/Date.test.js
@@ -78,6 +78,81 @@ module.exports = {
 
         dt3.should.not.equal(dt2);
 
+    },
+
+    // }}}
+    // {{{ 'test format#pattern1'
+
+    'test format#pattern1': function() {
+
+        var dt = new Date('1/10/2007 03:05:01 PM GMT-0600');
+
+        NX.Date.format(dt, 'Y-m-d').should.equal('2007-01-11');
+        NX.Date.format(dt, 'F j, Y, g:i a').should.equal('January 11, 2007, 6:05 am');
+        NX.Date.format(dt, 'l, \\t\\he jS \\of F Y h:i:s A').should.equal('Thursday, the 11th of January 2007 06:05:01 AM');
+        NX.Date.format(dt, 'c').should.equal('2007-01-11T06:05:01+09:00');
+        NX.Date.format(dt, 'MS').should.equal("\\/Date(1168463101000)\\/");
+
+    },
+
+    // }}}
+    // {{{ 'test parse#pattern1'
+
+    'test parse#pattern1': function() {
+
+        var dt = new Date();
+
+        dt = NX.Date.parse("2006", "Y");
+        NX.Date.format(dt, 'Y').should.equal('2006');
+
+        dt = NX.Date.parse("2011/01/01", "Y/m/d");
+        NX.Date.format(dt, 'Y/m/d').should.equal('2011/01/01');
+
+        dt = NX.Date.parse('00:01:00 m is month', "H:m:s \\m \\i\\s\ \\m\\o\\n\\t\\h");
+        NX.Date.format(dt, "H:m:s \\m \\i\\s\ \\m\\o\\n\\t\\h").should.equal('00:01:00 m is month');
+
+        dt = NX.Date.parse("2006-01-15", "Y-m-d");
+        NX.Date.format(dt, 'Y-m-d').should.equal('2006-01-15');
+
+        dt = NX.Date.parse("2006-01-15 3:20:01 PM", "Y-m-d g:i:s A");
+        NX.Date.format(dt, 'Y-m-d g:i:s A').should.equal('2006-01-15 3:20:01 PM');
+
+        dt = NX.Date.parse("2006-02-29 03:20:01", "Y-m-d H:i:s", true);
+        assert.equal(dt, null);
+
+    },
+
+    // }}}
+    // {{{ 'test getFormatCode#pattern1'
+
+    'test getFormatCode#pattern1': function() {
+
+        NX.Date.formatCodes.x = "NX.util.Format.leftPad(this.getDate(), 2, '0')";
+
+        NX.Date.format(new Date('2006-01-01'), 'X').should.equal('X');
+        NX.Date.format(new Date('2006-01-01'), 'x').should.equal('01');
+
+    },
+
+    // }}}
+    // {{{ 'test formatContainsDateInfo#pattern1'
+
+    'test formatContainsDateInfo#pattern1': function() {
+
+        NX.Date.formatContainsDateInfo('2006-01-01').should.equal(false);
+        NX.Date.formatContainsDateInfo('Y-m-d').should.equal(true);
+
+    },
+
+    // }}}
+    // {{{ 'test formatContainsHourInfo#pattern1'
+
+    'test formatContainsHourInfo#pattern1': function() {
+
+        NX.Date.formatContainsHourInfo('2006-01-01').should.equal(false);
+        NX.Date.formatContainsHourInfo('Y-m-d').should.equal(false);
+        NX.Date.formatContainsHourInfo('Y-m-d h:i:s').should.equal(true);
+
     }
 
     // }}}
