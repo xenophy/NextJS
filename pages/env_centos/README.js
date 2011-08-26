a527a5eefa074a39b1a4a67c2f0c0d701dc466ce
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-08-27 06:24:38 +0900

    CentOSインストール書き始めました。

--- pages/env_centos/README.js
+++ pages/env_centos/README.js
@@ -1,9 +1,27 @@
 Ext.data.JsonP.env_centos({
     "guide": [
         '<h1>Next JS 環境構築ガイド</h1>',
-        '<h2>Cent OS</h2>',
+        '<h2>CentOS(The Community ENTerprise Operating System)</h2>',
+        '<h3>開発環境構築</h3>',
         '<p>',
-        '',
-        '</p>'
+        'まず、CentOSをインストールする必要があります。',
+        'ここでは、CentOS 6を使ってインストール手順を紹介します。',
+        'CentOS自体は、<a href="http://www.centos.org/modules/tinycontent/index.php?id=32">ミラーリスト一覧</a>から選択してダウンロードしてください。',
+        '</p>',
+        '<h3>インストールと初期設定</h3>',
+        '<p>',
+        'ディスクを挿入して、サーバーを起動すると、次のような画面が表示されます。',
+        '</p>',
+        '<p><strong>Step.1</strong></p>',
+        '<img src="pages/env_centos/images/step1.jpg" />',
+        '<p><em>Install or upgrade an existing system</em>を選択して進みます。</p>',
+        '<br />',
+        '<p><strong>Step.2</strong></p>',
+        '<img src="pages/env_centos/images/step2.jpg" />',
+        '<p>インストールディスクのチェックを行う場合は、<em>OK</em>を選択します。通常は必要ないで<em>Skip</em>を選択し、次に進みます。</p>',
+
+
+
+
     ].join("")
 });
--- pages/env_overview/README.js
+++ pages/env_overview/README.js
@@ -3,7 +3,15 @@ Ext.data.JsonP.env_overview({
         '<h1>Next JS 環境構築ガイド</h1>',
         '<h2>概要</h2>',
         '<p>',
-        'Next JS を動作させる環境は、様々でしょう。',
-        '</p>'
+        'Next JS を動作させる環境は様々でしょう。',
+        'ここでは、Next JSを利用するために、開発環境と運用環境の構築手順をOSごとに紹介します。',
+        '</p>',
+        '<ul>',
+        '  <li><a href="#/pages/env_centos">CentOS(The Community ENTerprise Operating System)</a></li>',
+        '  <li>Max OS X</li>',
+        '  <li>ubuntu</li>',
+        '  <li>Scientific Linux</li>',
+        '  <li>openSUSE</li>',
+        '</ul>',
     ].join("")
 });
