6791af2854d08b39c6d01a4d32939255607a5e4b
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-09-11 06:57:25 +0900

    メール執筆中

--- pages/tutorial_mail/README.js
+++ pages/tutorial_mail/README.js
@@ -3,7 +3,13 @@ Ext.data.JsonP.tutorial_mail({
 
         '<h1>メール送信</h1>',
         '<p>',
+        'nodeを使ってメールを送信するためには、SMTPサーバーとの対話を自作するか、またはそのようなモジュールを入手する必要があります。',
+        'このNext JSを作っている私は、日本人です。',
+        'メール送信に関して、メールが送れるだけでなく、日本語にもきっちり対応したメール送信機能をNext JSは備えています。',
+        '題名や、送信先に日本語が入っていても大丈夫です。もちろん本文の日本語も正しく送信します。',
+        'では、その使い方を見ていきましょう。',
         '</p>',
+
 
         '<address>Copyright &copy; 2006 - 2011 <a href="http://www.xenophy.com/">Xenophy.CO.,LTD</a> All rights Reserved.</address>',
 
